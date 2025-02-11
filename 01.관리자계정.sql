5d447219fa18870cf8199f2055b356090dd2c4c8
tdchoi <ctd2580@naver.com>
2025-02-11 18:19:39 +0900

    oracle(250211)

--- 01.관리자계정.sql
+++ 01.관리자계정.sql
@@ -48,3 +48,15 @@ ALTER SESSION SET "_oracle_script" = true;
 CREATE USER scott IDENTIFIED BY 1234;
 GRANT CONNECT, RESOURCE TO scott;
 ALTER USER scott default tablespace users quota unlimited on users;
+
+-- ddl사용자 만들기
+ALTER SESSION SET "_oracle_script" = true;
+CREATE USER ddl IDENTIFIED BY 1234;
+GRANT CONNECT, RESOURCE TO ddl;
+ALTER USER ddl default tablespace users quota unlimited on users;
+
+-- worksheet사용자 만들기
+ALTER SESSION SET "_oracle_script" = true;
+CREATE USER worksheet IDENTIFIED BY 1234;
+GRANT CONNECT, RESOURCE TO worksheet;
+ALTER USER worksheet default tablespace users quota unlimited on users;
